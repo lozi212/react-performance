5e6d9936da732b47756b15db02433b1ee966686b
Loza Demeke <your-github-email@example.com>
2026-09-17 17:56:06 +0300

    Complete exercise 05

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -21,12 +21,15 @@ function appReducer(state, action) {
     case 'TYPED_IN_DOG_INPUT': {
       return {...state, dogName: action.dogName}
     }
+
     case 'UPDATE_GRID_CELL': {
       return {...state, grid: updateGridCellState(state.grid, action)}
     }
+
     case 'UPDATE_GRID': {
       return {...state, grid: updateGridState(state.grid)}
     }
+
     default: {
       throw new Error(`Unhandled action type: ${action.type}`)
     }
@@ -38,8 +41,10 @@ function AppProvider({children}) {
     dogName: '',
     grid: initialGrid,
   })
+
   // 🐨 memoize this value with React.useMemo
-  const value = [state, dispatch]
+  const value = React.useMemo(() => [state, dispatch], [state, dispatch])
+
   return (
     <AppStateContext.Provider value={value}>
       {children}
@@ -49,9 +54,11 @@ function AppProvider({children}) {
 
 function useAppState() {
   const context = React.useContext(AppStateContext)
+
   if (!context) {
     throw new Error('useAppState must be used within the AppProvider')
   }
+
   return context
 }
 
@@ -59,7 +66,9 @@ function Grid() {
   const [, dispatch] = useAppState()
   const [rows, setRows] = useDebouncedState(50)
   const [columns, setColumns] = useDebouncedState(50)
+
   const updateGridData = () => dispatch({type: 'UPDATE_GRID'})
+
   return (
     <AppGrid
       onUpdateGrid={updateGridData}
@@ -71,12 +80,20 @@ function Grid() {
     />
   )
 }
+
 Grid = React.memo(Grid)
 
 function Cell({row, column}) {
   const [state, dispatch] = useAppState()
   const cell = state.grid[row][column]
-  const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
+
+  const handleClick = () =>
+    dispatch({
+      type: 'UPDATE_GRID_CELL',
+      row,
+      column,
+    })
+
   return (
     <button
       className="cell"
@@ -90,6 +107,7 @@ function Cell({row, column}) {
     </button>
   )
 }
+
 Cell = React.memo(Cell)
 
 function DogNameInput() {
@@ -104,12 +122,14 @@ function DogNameInput() {
   return (
     <form onSubmit={e => e.preventDefault()}>
       <label htmlFor="dogName">Dog Name</label>
+
       <input
         value={dogName}
         onChange={handleChange}
         id="dogName"
         placeholder="Toto"
       />
+
       {dogName ? (
         <div>
           <strong>{dogName}</strong>, I've a feeling we're not in Kansas anymore
@@ -121,9 +141,11 @@ function DogNameInput() {
 
 function App() {
   const forceRerender = useForceRerender()
+
   return (
     <div className="grid-app">
       <button onClick={forceRerender}>force rerender</button>
+
       <AppProvider>
         <div>
           <DogNameInput />
